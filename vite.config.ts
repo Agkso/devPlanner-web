eb0df673bcd14d830c7223f620bba53cf2e80332
Augusto <augusto.k.ogawa@gmail.com>
2025-11-09 12:07:05 -0300

    feat: correction pipe

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import fs from "node:fs";
 import path from "path";
 import { defineConfig } from "vite";
-import { vitePluginManusRuntime } from "vite-plugin-manus-runtime";
 
-const plugins = [react(), tailwindcss(), vitePluginManusRuntime()];
+const plugins = [react()];
 
 export default defineConfig({
   plugins,
